0e7fb0d157cf38934735e327468b9d5fefb07d0d
guerlielton <guerlielton@gmail.com>
2024-10-23 22:38:32 +0100

    add: namespace prod istio labels

--- main.py
+++
@@ -1,6 +0,0 @@
-import sys # Import the sys module to access command-line arguments
-
-def main(): # Define the main function
-    if len(sys.argv) != 2: # Check if the number of command-line arguments is not equal to 2
-        print("Usage: python script.py <number>") # Print the usage message
-        sys.exit(1) # Exit the program with an error code
