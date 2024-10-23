02851d27846aa4c5c29b10e08d773b45e6010b12
guerlielton <guerlielton@gmail.com>
2024-10-23 23:16:00 +0100

    add: helm values example

--- terraform/helm/sonarqube.tf
+++ terraform/helm/sonarqube.tf
@@ -6,7 +6,7 @@ resource "helm_release" "sonarqube" {
   namespace        = "sonarqube"
   create_namespace = true
   version          = "10.7.0+3598" # Chart Version
-  values           = [file("${path.module}/sonarqube/values.yaml")]
+  values           = [file("${path.module}/helm-values-example/sonarqube/values.yaml")]
  
   lifecycle {
     ignore_changes = [values,metadata]
